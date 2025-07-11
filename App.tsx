06630eb7d63d8bc8633542424c3ba86b5d4e81f8
DanielDemico <135667661+DanielDemico@users.noreply.github.com>
2025-07-11 15:56:46 -0300

    add vercel feat

--- App.tsx
+++ App.tsx
@@ -8,6 +8,7 @@ import AboutPage from './pages/AboutPage';
 import ContactPage from './pages/ContactPage';
 import PrivacyPolicyPage from './pages/PrivacyPolicyPage';
 import ScrollToTop from './components/ScrollToTop';
+import { Analytics } from "@vercel/analytics/next"
 
 function App(): React.ReactNode {
   return (
